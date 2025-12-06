fce420ca7b18b7b0818bc6082d7dabea2ff558ca
Ana Julia <anajuliadefendi@gmail.com>
2025-12-06 04:25:00 -0300

    Dias das sessões mostrados conforme dia atual
    Para mostrar as opções de sessões, verifica-se o dia e mês atual. Os horários conseguem ser selecionados. Inseri um link para a sessão assentos e os quadradinhos que indicam as sessões agora estão responsivos.

--- cinema/src/app/sessoes/page.js
+++ cinema/src/app/sessoes/page.js
@@ -1,112 +1,140 @@
-export default function Sessoes(){
-    return(
-        <div className="min-h-[calc(100vh-110px)] w-full flex-col flex items-center">
-            <div>
-                <div className="flex mt-5 w-full justify-center">
-                    <button className="flex bg-[#a60301] text-white w-[60px] h-[60px] rounded-xl items-center justify-center font-bold">
-                        <p className="font-bold text-2xl">1</p>
-                    </button>
-                    <div className="border-t-2 border-[#545454] w-[100px] flex self-center"></div>
+"use client";
+import { useState } from "react";
+import Link from "next/link";
 
-                    <button className="flex bg-[#a60301] text-white w-[60px] h-[60px] rounded-xl items-center justify-center font-bold">
-                        <p className="font-bold text-2xl">2</p>
-                    </button>
-                    <div className="border-t-2 border-[#545454] w-[100px] flex self-center"></div>
+export default function Sessoes() {
+    const diasSemana = ["Dom.", "Seg.", "Ter.", "Qua.", "Qui.", "Sex.", "Sáb."];
 
-                    <button className="flex bg-[#a60301] text-white w-[60px] h-[60px] rounded-xl items-center justify-center font-bold">
-                        <p className="font-bold text-2xl">3</p>
-                    </button>
-                    <div className="border-t-2 border-[#545454] w-[100px] flex self-center"></div>
+    const hoje = new Date();
 
-                    <button className="flex bg-[#a60301] text-white w-[60px] h-[60px] rounded-xl items-center justify-center font-bold">
-                        <p className="font-bold text-2xl">4</p>
-                    </button>
-                    <div className="border-t-2 border-[#545454] w-[100px] flex self-center"></div>
+    const proximos7Dias = Array.from({ length: 7 }, (_, i) => {
+        const data = new Date();
+        data.setDate(hoje.getDate() + i);
 
-                    <button className="flex bg-[#a60301] text-white w-[60px] h-[60px] rounded-xl items-center justify-center font-bold">
-                        <p className="font-bold text-2xl">5</p>
-                    </button>
-                    <div className="border-t-2 border-[#545454] w-[100px] flex self-center"></div>
+        const dia = String(data.getDate()).padStart(2, "0");
+        const semana = diasSemana[data.getDay()];
 
-                    <button className="flex bg-[#a60301] text-white w-[60px] h-[60px] rounded-xl items-center justify-center font-bold">
-                        <p className="font-bold text-2xl">6</p>
-                    </button>
-                </div>
-                <div className="flex w-full justify-center">
-                    <div className="w-40 text-center font-bold">Sessões</div>
-                    <div className="w-40 text-center font-bold">Assentos</div>
-                    <div className="w-40 text-center font-bold">Ingressos</div>
-                    <div className="w-40 text-center font-bold">Bomboniere</div>
-                    <div className="w-40 text-center font-bold">Pagamento</div>
-                    <div className="w-40 text-center font-bold">Confirmação</div>
-                </div>
+        return { dia, semana };
+    });
+
+    const meses = ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"];
+
+    const mesAtual = meses[hoje.getMonth()];
+
+    const [diaSelecionado, setDiaSelecionado] = useState(0);
+
+    const [horarioSelecionado, setHorarioSelecionado] = useState(null);
+
+    const horarios = [
+        { id: 1, hora: "15:30", tipo: ["DUB", "2D"] },
+        { id: 2, hora: "19:30", tipo: ["DUB", "2D"] }
+    ];
+
+
+    return (
+        <div className="min-h-[calc(100vh-110px)] w-full flex-col flex items-center px-2 sm:px-4">
+            {/* ETAPAS */}
+            <div className="flex mt-5 w-full justify-center flex-wrap gap-y-2">
+
+                {[1, 2, 3, 4, 5, 6].map((n, i) => (
+                    <>
+                        <button
+                            className={'flex text-white w-9 h-9 sm:w-[50px] sm:h-[50px] lg:w-[60px] lg:h-[60px] rounded-xl items-center justify-center font-bold bg-[#a60301]'}
+                        >
+                            <p className="font-bold text-base sm:text-xl lg:text-2xl">{n}</p>
+                        </button>
+
+                        {n < 6 && (
+                            <div className="border-t-2 border-[#545454] w-[35px] sm:w-[70px] lg:w-[100px] flex self-center"></div>
+                        )}
+                    </>
+                ))}
+
             </div>
+
+            {/* TÍTULOS */}
+            <div className="flex w-full justify-center overflow-x-auto">
+                {["Sessões", "Assentos", "Ingressos", "Bomboniere", "Pagamento", "Confirmação"].map((t) => (
+                    <div key={t} className="w-[90px] sm:w-40 text-center font-bold text-xs sm:text-base">
+                        {t}
+                    </div>
+                ))}
+            </div>
+
+            {/* Avançar para assentos */}
+            <div className="flex flex-wrap justify-end w-full mt-4 gap-3">
+                <Link className="w-[18%] mr-10" href="/assentos">
+                    <button className="bg-[#a60301] text-white font-bold w-full h-12 rounded-full cursor-pointer transition-all duration-200 hover:bg-[#c90401] hover:scale-105">
+                        Continuar para assentos
+                    </button>
+                </Link>
+            </div>
+
             <div className="flex w-full mt-16">
                 <div className="flex w-[40%] ml-10">
                     <img src="/interestelar.jpeg" alt="" className="rounded-lg mb-4" />
                     <div className="ml-5">
                         <p className="font-bold text-xl flex items-center">
-                            Interestelar 
+                            Interestelar
                             <div className="bg-[#008000] rounded-lg w-8 h-8 flex items-center justify-center text-white font-bold ml-2">L</div>
                         </p>
                         <p className="">Ficção</p>
                         <p className="">120min</p>
                     </div>
-                    
+
                 </div>
                 <div className="flex flex-col w-[60%]">
                     <h1 className="font-bold text-xl">Escolha uma sessão</h1>
                     <div className="flex border border-[#a6a6a6] w-[80%] h-[28%] rounded-xl mt-3 items-center">
-                        <div className="flex bg-[#d9d9d9] w-[9%] h-[80%] ml-2.5 rounded-xl items-center justify-center">OUT</div>
+                        <div className="flex bg-[#d9d9d9] w-[9%] h-[80%] ml-2.5 rounded-xl items-center justify-center">
+                            {mesAtual}
+                        </div>
                         <div className="flex w-[90%] h-full rounded-xl items-center justify-around mx-8">
-                            <button className="flex bg-[#a60301] text-white w-[10%] h-[80%] rounded-xl items-center justify-center font-bold">
-                                1 <br></br>
-                                Qua.
-                            </button>
-                            <button className="flex w-[10%] h-[80%] rounded-xl items-center justify-center">
-                                2 <br></br>
-                                Qui.
-                            </button>
-                            <button className="flex w-[10%] h-[80%] rounded-xl items-center justify-center">
-                                3 <br></br>
-                                Sex.
-                            </button>
-                            <button className="flex w-[10%] h-[80%] rounded-xl items-center justify-center">
-                                4 <br></br>
-                                Sáb.
-                            </button>
-                            <button className="flex w-[10%] h-[80%] rounded-xl items-center justify-center">
-                                5 <br></br>
-                                Dom.
-                            </button>
-                            <button className="flex w-[10%] h-[80%] rounded-xl items-center justify-center">
-                                6 <br></br>
-                                Seg.
-                            </button>
-                            <button className="flex w-[10%] h-[80%] rounded-xl items-center justify-center">
-                                7 <br></br>
-                                Ter.
-                            </button>
+                            {proximos7Dias.map((item, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => setDiaSelecionado(index)}
+                                    className={`flex w-[10%] h-[80%] rounded-xl items-center justify-center transition-all duration-200 cursor-pointer ${diaSelecionado === index
+                                        ? "bg-[#a60301] font-bold text-white"
+                                        : "bg-transparent text-black"
+                                        }`}
+                                >
+                                    {item.dia}
+                                    <br />
+                                    {item.semana}
+                                </button>
+                            ))}
+
                         </div>
                     </div>
                     <div className="border border-[#a6a6a6] w-[80%] h-[40%] rounded-xl mt-5 flex items-center">
                         <div className="ml-2.5 border-l-2 border-[#a60301] h-[72px] flex items-center">
                             <p className="font-bold text-xl ml-3">Sala 1</p>
                         </div>
-                        <button className="flex w-[20%] h-[80%] border border-[#a6a6a6] rounded-xl items-center justify-center flex-col ml-10 cursor-pointer">
-                            <p>15h30</p>
-                            <div className="flex w-full h-[50%] items-center justify-around">
-                                <div className="bg-[#ffd900] rounded-xl w-[40%] h-[90%] flex items-center justify-center text-white font-bold">DUB</div>
-                                <div className="bg-[#ffd900] rounded-xl w-[40%] h-[90%] flex items-center justify-center text-white font-bold">2D</div>
-                            </div>
-                        </button>
-                        <button className="flex w-[20%] h-[80%] rounded-xl items-center justify-center flex-col ml-10 cursor-pointer">
-                            <p>19h30</p>
-                            <div className="flex w-full h-[50%] items-center justify-around">
-                                <div className="bg-[#ffd900] rounded-xl w-[40%] h-[90%] flex items-center justify-center text-white font-bold">DUB</div>
-                                <div className="bg-[#ffd900] rounded-xl w-[40%] h-[90%] flex items-center justify-center text-white font-bold">2D</div>
-                            </div>
-                        </button>
+                        {horarios.map((horario) => (
+                            <button
+                                key={horario.id}
+                                onClick={() => setHorarioSelecionado(horario.hora)}
+                                className={`flex w-[20%] h-[80%] rounded-xl items-center justify-center flex-col ml-10 cursor-pointer transition-all ${horarioSelecionado === horario.hora
+                                        ? "border border-[#a6a6a6] scale-105"
+                                        : ""
+                                    }`}
+                            >
+                                <p>{horario.hora}</p>
+
+                                <div className="flex w-full h-[50%] items-center justify-around">
+                                    {horario.tipo.map((tipo, index) => (
+                                        <div
+                                            key={index}
+                                            className="bg-[#ffd900] rounded-xl w-[40%] h-[90%] flex items-center justify-center text-white font-bold"
+                                        >
+                                            {tipo}
+                                        </div>
+                                    ))}
+                                </div>
+                            </button>
+                        ))}
+
                     </div>
                 </div>
             </div>
